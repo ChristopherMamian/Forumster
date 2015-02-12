21b2edefa0f016497e6952e232cc9378127c334b
Christopher Mamian <christophermamian@gmail.com>
2015-02-12 12:07:31 -0800

    Authorization on links

--- raddit/app/controllers/links_controller.rb
+++ raddit/app/controllers/links_controller.rb
@@ -1,6 +1,7 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-
+  before_action :authorized_user, only: [:edit, :update, :destroy]
+  before_filter :authenticate_user!, :except => [:index, :show]
   # GET /links
   # GET /links.json
   def index
@@ -14,7 +15,7 @@ class LinksController < ApplicationController
 
   # GET /links/new
   def new
-    @link = Link.new
+    @link = current_user.links.build
   end
 
   # GET /links/1/edit
@@ -24,7 +25,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
+    @link = current_user.links.build(link_params)
 
     respond_to do |format|
       if @link.save
@@ -63,6 +64,11 @@ class LinksController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def authorized_user
+      @link = current_user.links.find_by(id: params[:id])
+      redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
+    end
+
     def set_link
       @link = Link.find(params[:id])
     end
